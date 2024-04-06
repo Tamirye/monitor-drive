5c03f492282b21aa0e92b1f7010466a56fdd74b3
Tamirye <34610125+Tamirye@users.noreply.github.com>
2024-04-06 19:41:01 +0300

    added script v1

---
+++ README.md
@@ -0,0 +1,101 @@
+# Google Drive Monitor
+
+This Python script is designed enumerate the enforced Google Drive file sharing settings in a given Google account, find all publicly accessible folders and monitor them for new files. If a new file is added to a publicly accessible folder, the file permissions are adjusted to keep it private.
+
+## Prerequisites
+
+- **Python Environment**: Ensure Python 3.7 or higher is installed on your system.
+
+Before running this script, you must have an enabled Google Drive API and a credentials file to connect to it. If you don't have those,
+Complete the following steps to enable the Google Drive API and create the necessary credentials:
+
+1. **Google Account**: Ensure you have a Google account, if not, create one:
+
+   - Navigate to [Create Google Account Page](https://accounts.google.com/signup) and create an account.
+
+2. **Google Cloud Project**: Ensure you have a Google Cloud Project. If not, create one:
+
+   - Navigate to the [Google API Console](https://console.developers.google.com/) and choose your organization (if you are a part of one).
+   - Create a new project, if you are restricted from doing so, contact your administrators.
+
+3. **Enable the Google Drive API**:
+
+   - Navigate to the [Google API Console](https://console.developers.google.com/).
+   - Select your project.
+   - Click on “Library”.
+   - Search for “Google Drive API”, select it, and click “Enable”.
+
+4. **Create OAuth Consent**:
+
+   - Navigate to the [Google API Console](https://console.developers.google.com/).
+   - Select your project.
+   - Click on “OAuth consent screen".
+   - For User type select Internal for account that is a member of an organization (or Google Workspace), and External otherwise. Then click Create.
+   - Complete the app registration form, then click Save and Continue.
+   - Add the authorization scopes that the application requires (/auth/drive).
+
+5. **Create Credentials**:
+   - Navigate to the [Google API Console](https://console.developers.google.com/).
+   - Select your project.
+   - Click on “Credentials”.
+   - Click “Create Credentials” and select “OAuth client ID”.
+   - Choose the application type “Desktop app” and give it a name.
+   - Once created, you can download the credentials file by clicking the download button (JSON format).
+
+After completing these steps, you should have a `credentials.json` file, which is required to authenticate and authorize the script to access your Google Drive.
+
+#### Installation
+
+1. **Clone the project**:
+   - `git clone https://github.com/Tamirye/drive-monitor`
+2. **Install Dependencies**:
+   - Install the required Python packages by running:
+     ```
+     pip install -r requirements.txt
+     ```
+
+#### Usage
+
+1. **Script Configuration**:
+   - Place the `credentials.json` file in the same directory as the script, or specify its path when running the script.
+2. **Running the Script**:
+
+   - To run the script, use the following command in the terminal, replacing `<credentials_file>` with the path to your credentials file:
+     ```
+     python google_drive_monitor.py -c <credentials_file>
+     ```
+   - Optionally, you can specify a token file (which stores your user's session tokens) using the `-t` or `--token_file` argument. This is useful for avoiding repeated authorizations.
+
+   ```
+   python google_drive_monitor.py -c path/to/your/credentials.json -t path/to/your/token.json
+
+   ```
+
+   - Optionally, for testing the enforced sharing settings you can use the following options:
+     - `-e`, `--test_email`: Email address used to test sharing options. Default is `email@example.com`.
+     - `-d`, `--test_domain`: Domain to test sharing options against. Default is `google.com`.
+     - `-g`, `--test_group_id`: Group ID to test sharing options against. Default is `testvaronis123@googlegroups.com`.
+
+   ```
+   python google_drive_monitor.py -c path/to/credentials.json -e user@example.com -d example.com -g yourgroup@example.com
+   ```
+
+   - **Notes on Sharing Settings Enumeration**:
+
+     - Admin users in the Google Suite can view the directly using the admin SDK, as I didn't have Google Suite license and I couldn't test this functionality, I decided to test localy by trying to share a file in different scenarios using the given user permissions. This may produce different results depending on the user's permissions and could not be relied for organization wide policies.
+     - Changing ownership on a file via API is restricted so it is not tested
+     - The default email was chosen as an email that is defently not part of the google account domain
+     - The reason I chose google.com as a default domain is that I know that they are using Google Suite so I can be sure that the domain is eligible for sharing.
+     - Groups cannot be tested without a group id so I decided to use a group id I own on a different google account then the one I tested the script on.
+
+   - Optionally, the user can adjust the sleep time between file monitoring scaning operations so not to exceed the Google Drive API rate limit. This can be done using `-s`, `--sleep_period` Sleep period (in seconds). Default is 10 seconds.
+
+   ```
+   python google_drive_monitor.py -c path/to/credentials.json -s 30
+
+   ```
+
+#### Important Notes
+
+- **Rate Limits**: The Google Drive API has usage limits; excessive requests may lead to temporary blocking.
+- **Permission Adjustments**: The script adjusts permissions for new files only; existing file permissions are not modified.
---
+++ monitor-drive.py
@@ -0,0 +1,253 @@
+import argparse
+import json
+import os.path
+import string
+import random
+import time
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+
+SCOPES = ["https://www.googleapis.com/auth/drive"]
+
+
+def art():
+    return """
+             ___________________
+            /                   \\
+           /     ___     ___     \\
+          /     /   \\   /   \\     \\
+         /     /     \\ /     \\     \\
+        /     /_______V_______\\     \\
+       /     /  \\           /  \\     \\
+      /     /    \\         /    \\     \\
+     /     /      \\       /      \\     \\
+    /     /        \\     /        \\     \\
+   /     /__________\\   /__________\\     \\
+  /                                       \\
+ /           Google Drive Monitor          \\
+/               By Tamir Yehuda             \\
+\\___________________________________________/
+
+"""
+
+
+def connect_to_drive_api(credentials_file, token_file=None):
+    creds = None
+    if token_file and os.path.exists(token_file):
+        creds = Credentials.from_authorized_user_file(token_file, SCOPES)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                credentials_file, SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open("token.json", 'w') as token:
+            token.write(creds.to_json())
+    return build('drive', 'v3', credentials=creds)
+
+
+def generate_random_string():
+    """Generate a random string of fixed length."""
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(10))
+
+
+def enumerate_sharing_settings(session, test_email, test_domain, test_group_id):
+    """
+        Create a test file and try different sharing settings to determine the current set sharing settings.
+    """
+    file_name = f"testfile-{generate_random_string()}"
+    file_metadata = {
+        'name': f'{file_name}',
+        'mimeType': 'text/plain'
+    }
+    try:
+        print(
+            "[!] Creating a test file to enumerate available sharing configurations...")
+        file = session.files().create(body=file_metadata, fields='id').execute()
+        file_id = file.get('id')
+        print(f"[+] Created file {file_name} with ID {file_id}")
+        print(
+            f"[!] Testing sharing settings against Email: {test_email}, Domain: {test_domain}, Group ID: {test_group_id}")
+
+        types_and_roles = [
+            ('user', 'reader'),
+            ('user', 'commenter'),
+            ('user', 'writer'),
+            ('domain', 'reader'),
+            ('domain', 'commenter'),
+            ('domain', 'writer'),
+            ('anyone', 'reader'),
+            ('anyone', 'commenter'),
+            ('anyone', 'writer'),
+            ('group', 'reader'),
+            ('group', 'commenter'),
+            ('group', 'writer')
+        ]
+
+        sharing_results = []
+        for type_, role in types_and_roles:
+            body = {
+                'type': type_,
+                'role': role,
+                'emailAddress': test_email if type_ in ['user'] else None,
+                'domain': test_domain if type_ == 'domain' else None
+            }
+            if type_ == 'group':
+                body['emailAddress'] = test_group_id
+
+            try:
+                session.permissions().create(
+                    fileId=file_id,
+                    body={k: v for k, v in body.items() if v is not None},
+                    fields='id').execute()
+                sharing_results.append(
+                    f"[+] Success: {type_} with role {role}")
+            except HttpError as error:
+                error_content = error.content.decode("utf-8")
+                error_json = json.loads(error_content)
+                error_message = error_json.get("error").get("errors")[
+                    0].get("message")
+                sharing_results.append(
+                    f"[-] Failed: {type_} with role {role} - {error_message}")
+
+        print("[+] Enumerated sharing policy:")
+        print("-------------------------------------")
+        for result in sharing_results:
+            print(result)
+        print("-------------------------------------")
+
+    except HttpError as error:
+        print(f'[-] An error occurred: {error}')
+
+    try:
+        session.files().delete(fileId=file_id).execute()
+        print(f"[+] Deleted test file {file_id}")
+    except HttpError as error:
+        print(f"[-] Failed to delete test file {file_id}: {error}")
+
+
+def enumerate_publicly_accessible_folders(session):
+    """
+        List all folders and check for each folder if it is publicly accessible.
+        Returns a list of publicly accessible folders.
+    """
+    try:
+        print("[+] Enumerating publicly accessible folders....")
+        query = "mimeType = 'application/vnd.google-apps.folder' and trashed = false"
+        results = session.files().list(q=query,
+                                       spaces='drive',
+                                       fields='nextPageToken, files(id, name, permissions)').execute()
+        folders = results.get('files', [])
+        public_folders = []
+
+        for folder in folders:
+            for permission in folder.get('permissions', []):
+                if permission.get('type') == 'anyone':
+                    print(
+                        f"[!] Folder {folder['name']} ({folder['id']}) is publicly accessible, adding it to watch list")
+                    public_folders.append(folder)
+                    break
+
+        return public_folders
+    except HttpError as error:
+        print(f'An error occurred: {error}')
+        return []
+
+
+def monitor_public_folders_change_new_file_permissions(session, folders, sleep_period):
+    """
+        Monitor specified folders and adjust permissions of new files to private.
+    """
+    known_files = {}
+    for folder in folders:
+        known_files[folder['id']] = []
+        query = f"'{folder['id']}' in parents and trashed = false"
+        results = session.files().list(q=query,
+                                       spaces='drive',
+                                       fields='nextPageToken, files(id, name, permissions)').execute()
+        files = results.get('files', [])
+        for file in files:
+            known_files[folder['id']].append(file['id'])
+
+    while True:
+        try:
+            for folder in folders:
+                query = f"'{folder['id']}' in parents and trashed = false"
+                results = session.files().list(q=query,
+                                               spaces='drive',
+                                               fields='nextPageToken, files(id, name, permissions)').execute()
+                files = results.get('files', [])
+
+                for file in files:
+                    if file['id'] not in known_files[folder['id']]:
+
+                        print(
+                            f"[!] New file detected: {file['name']} in folder: {folder['name']}")
+                        for permission in file.get('permissions', []):
+                            if permission.get('type') == 'anyone':
+                                try:
+                                    session.permissions().delete(
+                                        fileId=file['id'], permissionId=permission['id']).execute()
+                                    print(
+                                        f"[+] Made file private: {file['name']} ({file['id']})")
+                                except HttpError as error:
+                                    print(
+                                        f"[-] Failed to change permission for {file['name']}: {error}")
+                        known_files[folder['id']].append(file['id'])
+
+            time.sleep(sleep_period)
+
+        except HttpError as error:
+            print(f'[-] An error occurred: {error}')
+
+
+def main():
+    try:
+        print(art())
+        parser = argparse.ArgumentParser(
+            description='Enumerate Google Drive enforced sharing permissions and public accessible folders. monitor public accessible folders and adjust access permissions for new files inisde them to private.')
+        parser.add_argument('-c', '--credentials_file', type=str,
+                            required=True, help='Path to the credentials file')
+        parser.add_argument('-t', '--token_file', type=str,
+                            help='Path to the token file (optional)')
+        parser.add_argument('-e', '--test_email', type=str,
+                            help='Email address to test sharing options against (optional)', default='email@example.com')
+        parser.add_argument('-d', '--test_domain', type=str,
+                            help='domain to test sharing options against (optional)', default='google.com')
+        parser.add_argument('-g', '--test_group_id', type=str,
+                            help='group id to test sharing options against (optional)', default='testvaronis123@googlegroups.com')
+        parser.add_argument('-s', '--sleep_period', type=int,
+                            help='sleep period for new file creation monitoring, default is 10 seconds. Adjust to reduce API calls according to rate limit (optional)', default=10)
+
+        args = parser.parse_args()
+        if os.path.exists(args.credentials_file):
+            session = connect_to_drive_api(
+                args.credentials_file, args.token_file)
+            enumerate_sharing_settings(
+                session, args.test_email, args.test_domain, args.test_group_id)
+            public_folders = enumerate_publicly_accessible_folders(session)
+            print(
+                f"[+] Found {len(public_folders)} publicly accessible folders to monitor.")
+            if public_folders:
+                monitor_public_folders_change_new_file_permissions(
+                    session, public_folders, args.sleep_period)
+            else:
+                print("[!] No publicly accessible folder found")
+                print("[-] Bye :)")
+        else:
+            print("[-] The credentials file path you supplied does not exist!")
+
+    except KeyboardInterrupt:
+        print("\n[!] Script interrupted by user. Exiting gracefully...")
+        print("[-] Bye :)")
+
+
+if __name__ == "__main__":
+    main()
